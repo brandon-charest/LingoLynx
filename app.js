8a84e5e712b68c4dffae79913be21625f10a25f3
EncRyptEd77 <brandon_charest@yahoo.com>
2016-02-02 21:57:59 -0500

    add get user and get users

--- app.js
+++ app.js
@@ -1,68 +1,75 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 
-
+//work area start
 var app = express();
+var mysql = require('mysql');
+//TODO: Move DB information to config file
+app.db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'LingoLynx'
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.db.connect(function (err) {
+    if (err) {
+        console.error('could not connect to DB', err);
+    } else {
+        console.log('Successfully connect to DB');
+    }
+});
+
+
+//routes start
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
 
 //routes
-var users = require('./routes/users')(app);
+require('./routes/users')(app);
 
-//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
+if (app.get('env') === 'development') {
+
+    app.use(function (err, req, res, next) {
+        console.log(err);
+        res.sendStatus(err.status || 500);
+        console.log('send error');
+    });
+}
 
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.sendStatus(err.status || 500);
+
 });
-
-
 
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,40 @@ var express = require('express');
 var router= function(app){
 
   app.get('/users', function(req, res, next) {
-    res.send('all users');
+    app.db.query('SELECT * FROM user', function(err, rows, feilds){
+      if (err) {
+        next(err);
+        return;
+      }
+      console.log(rows);
+      res.send(rows);
+    });
+
   });
 
   app.get('/users/:user_id', function(req, res, next) {
-    res.send('single user');
+    if(!req.params.user_id){
+      next('some error or somthing');
+      return;
+    }
+    app.db.query("SELECT * FROM user WHERE idUser=?",[req.params.user_id], function(err, rows, feilds){
+
+      if (err) {
+
+        next(err);
+        return;
+      }
+
+      if(rows && rows[0] && rows[0].idUser.toString()===req.params.user_id){
+
+        res.send(rows[0]);
+      }else{
+
+        next('No user matches that id');
+      }
+
+    });
+
   });
 
   app.post('/users', function(req, res, next) {
