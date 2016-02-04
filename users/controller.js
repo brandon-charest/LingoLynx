ddbe900da26960a2ccf1a8f08bb34f531d4556e0
Nickolas Freitas <n_freitas1@salemstate.edu>
2016-02-03 22:30:00 -0500

    add create user

--- users/controller.js
+++ users/controller.js
@@ -34,7 +34,60 @@ var usersController = function (app) {
     }
 
     function createUser(req, res, next) {
-        next('Not yet implemented');
+        var user = {};
+
+        var allRequiredPropertiedToCreateNewUserArePresent = true;
+        var missingRequiredPropertiesToCreateNewUser = [];
+        app.models.users.propertiesRequiredToCreateNewUser.forEach(function (propertyRequiredToCreateNewUser) {
+            if (!req.body[propertyRequiredToCreateNewUser]) {
+                allRequiredPropertiedToCreateNewUserArePresent = false;
+                missingRequiredPropertiesToCreateNewUser.push(propertyRequiredToCreateNewUser);
+            }
+        });
+
+        if (!allRequiredPropertiedToCreateNewUserArePresent) {
+            next('Missing the following required fields: ' + missingRequiredPropertiesToCreateNewUser);
+        }
+
+        //todo: make isActive default to true in schema-model
+        if(!user.isActive){
+            user.isActive = true;
+        }
+
+        app.models.users.encryptPassword(req.body.password)
+            .then(function (encryptedPassword) {
+                req.body.password = encryptedPassword;
+
+                app.models.users.propertiesThatCanBeSetWhenCreatingNewUser.forEach(function (userProperty) {
+                    if (req.body[userProperty]) {
+                        user[userProperty] = req.body[userProperty]
+                    }
+                });
+
+                app.db.query(app.models.users.createNewUser, [user], function (err, results) {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    app.db.query(app.models.users.getAllFromUserWhereIdUserMatches, [results.insertId], function (err, rows) {
+                        if (err) {
+                            next(err);
+                            return;
+                        }
+
+                        if (rows && rows[0] && rows[0].idUser && rows[0].idUser.toString() === results.insertId.toString()) {
+                            res.send(rows[0]);
+                            return;
+                        }
+
+                        next('Could not retrieve user after inserting it');
+                    });
+                });
+            })
+            .catch(function (err) {
+                next(err);
+            })
+            .done();
     }
 
     function updateUser(req, res, next) {
--- users/model.js
+++ users/model.js
@@ -1,8 +1,28 @@
+var q = require('q');
+
 var usersModel = function (app) {
+    function encryptPassword(unencryptedPassword) {
+        var deferred = q.defer();
+
+        //todo: encrypt password
+        var encryptedPassword = unencryptedPassword;
+
+        deferred.resolve(encryptedPassword);
+        return deferred.promise;
+    }
+
     return {
         //db queries
         getAllFromUser: "SELECT * FROM user",
-        getAllFromUserWhereIdUserMatches: "SELECT * FROM user WHERE idUser=?"
+        getAllFromUserWhereIdUserMatches: "SELECT * FROM user WHERE idUser=?",
+        createNewUser: "INSERT INTO user SET ?",
+
+        //record properties
+        propertiesThatCanBeSetWhenCreatingNewUser: ['username', 'password', 'email'],
+        propertiesRequiredToCreateNewUser: ['username', 'password', 'email'],
+
+        //functions
+        encryptPassword: encryptPassword
     };
 };
 
