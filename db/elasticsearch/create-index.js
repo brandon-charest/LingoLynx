21bd1ee420138ecf3c90546954e937d6cc54152d
Nickolas Freitas <n_freitas1@salemstate.edu>
2016-02-06 18:17:03 -0500

    set up index and mapper and data seed for sentences in elasticsearch

--- db/elasticsearch/create-index.js
+++ db/elasticsearch/create-index.js
@@ -1,12 +1,25 @@
 var es = require('elasticsearch');
+var async = require('async');
 
-var client = new es.Client({
+//config
+esClientConfig = {
     host: 'localhost:9200',
     log: 'trace'
-});
+};
 
-client.ping({}, function(err){
-    if(err){
+var indexName = 'lingo-lynx-sentences-';
+
+var languages = ['japanese', 'french'];
+var specialLanguageAnalyzers = {
+    japanese: 'kuromoji'
+};
+
+//end config
+
+var client = new es.Client(esClientConfig);
+
+client.ping({}, function (err) {
+    if (err) {
         console.error(err);
     } else {
         onESConnection();
@@ -14,6 +27,169 @@ client.ping({}, function(err){
 });
 
 //main
-function onESConnection(){
+function onESConnection() {
     console.log('Connected to ES');
+
+    var languageAnalyzers = {};
+    languages.forEach(function (language) {
+        if (specialLanguageAnalyzers[language]) {
+            languageAnalyzers[language] = specialLanguageAnalyzers[language];
+        }
+        else {
+            languageAnalyzers[language] = language;
+        }
+    });
+
+    var mappings = {};
+    languages.forEach(function (language) {
+        mappings[language] = {
+            properties: {
+                text: {
+                    type: 'string',
+                    fields: {
+                        stemmed: {
+                            type: 'string'
+                        }
+                    }
+                },
+                idUser: {
+                    type: 'long'
+                },
+                idLanguage: {
+                    type: 'long'
+                },
+                isActive: {
+                    type: 'boolean'
+                },
+                addedDate: {
+                    type: 'date'
+                },
+                lastModifiedDate: {
+                    type: 'date'
+                }
+            }
+
+        };
+        mappings[language].properties.text.fields.stemmed.analyzer = languageAnalyzers[language];
+    });
+
+    var indexExists = {};
+    async.series([
+            //check if indices exists
+            function (callback) {
+                async.each(languages,
+                    //iterator
+                    function (language, iteratorCallback) {
+                        client.indices.exists({
+                            index: indexName + language
+                        }, function (err, exists) {
+                            if (err) {
+                                iteratorCallback(err);
+                            } else {
+                                indexExists[language] = exists;
+                                iteratorCallback();
+                            }
+                        });
+                    },
+                    //final callback
+                    function (err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback();
+                        }
+                    }
+                );
+            },
+            //delete indices if they do
+            function (callback) {
+                async.each(languages,
+                    //iterator
+                    function (language, iteratorCallback) {
+                        if (!indexExists[language]) {
+                            iteratorCallback();
+                            return;
+                        }
+
+                        client.indices.delete({
+                            index: indexName + language
+                        }, function (err) {
+                            if (err) {
+                                iteratorCallback(err);
+                            } else {
+                                iteratorCallback();
+                            }
+                        });
+                    },
+                    //final callback
+                    function (err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback();
+                        }
+                    }
+                );
+            },
+            //create new indices
+            function (callback) {
+                async.each(languages,
+                    //iterator
+                    function (language, iteratorCallback) {
+                        client.indices.create({
+                            index: indexName + language
+                        }, function (err) {
+                            if (err) {
+                                iteratorCallback(err);
+                            } else {
+                                iteratorCallback();
+                            }
+                        });
+                    },
+                    //final callback
+                    function (err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback();
+                        }
+                    }
+                );
+            },
+            //update the mapping for each language
+            function (callback) {
+                async.each(languages,
+                    //iterator
+                    function (language, iteratorCallback) {
+                        client.indices.putMapping({
+                            index: indexName + language,
+                            type: 'sentence',
+                            body: mappings[language]
+                        }, function (err) {
+                            if (err) {
+                                iteratorCallback(err);
+                            } else {
+                                iteratorCallback();
+                            }
+                        });
+                    },
+                    //final callback
+                    function (err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback();
+                        }
+                    }
+                );
+            }
+        ],
+        function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Successfully created ', indexName, ' and updated all mappings');
+            }
+        }
+    );
 }
---
+++ db/elasticsearch/data-seed.js
@@ -0,0 +1,80 @@
+var es = require('elasticsearch');
+var async = require('async');
+
+process.stdin.setEncoding('utf8');
+process.stdout.setEncoding('utf8');
+
+//config
+esClientConfig = {
+    host: 'localhost:9200',
+    //log: 'trace'
+};
+
+var indexName = 'lingo-lynx-sentences-';
+
+var languages = ['japanese', 'french'];
+//end config
+
+var client = new es.Client(esClientConfig);
+
+client.ping({}, function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        onESConnection();
+    }
+});
+
+var dataSeeds = {
+    french: [
+        {
+            idUser: 1,
+            idLanguage: 2,
+            isActive: true,
+            text: "Bonjour le monde"
+        }
+    ],
+    japanese: [
+        {
+            idUser: 1,
+            idLanguage: 3,
+            isActive: true,
+            text: "こんにちは世界"
+        }
+    ]
+};
+
+console.log(dataSeeds.japanese[0]);
+
+//main
+function onESConnection() {
+    console.log('Connected to ES');
+
+    async.each(languages,
+        function (language, callback) {
+            if (dataSeeds[language] && dataSeeds[language].length) {
+                async.each(dataSeeds[language],
+                    function (dataSeed, innerCallback) {
+                        client.index({
+                                index: indexName + language,
+                                type: 'sentence',
+                                body: dataSeed
+                            }, function (err) {
+                                innerCallback(err);
+                            }
+                        )
+                    }, function (err) {
+                        callback(err);
+                    }
+                );
+            }
+        },
+        function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Successfully created ', indexName, ' and updated all mappings');
+            }
+        }
+    );
+}
