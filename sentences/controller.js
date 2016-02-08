1f2ec238375e1b8e92e6e9b634acf656aa2f9523
n-freitas <n_freitas1@salemstate.edu>
2016-02-07 22:14:22 -0500

    start implementing get sentence by id and create sentence

--- sentences/controller.js
+++ sentences/controller.js
@@ -17,7 +17,16 @@ var sentencesController = function (app) {
                     return;
                 }
 
-                if (typeof req.query.languages === 'string' || typeof req.query.languages === 'number') {
+                if (typeof req.query.languages === 'string') {
+                    try {
+                        req.query.languages = parseInt(req.query.languages);
+                    } catch (exception) {
+                        callback(exception);
+                        return;
+                    }
+                }
+
+                if (typeof req.query.languages === 'number') {
                     req.query.languages = [req.query.languages];
                 }
 
@@ -79,7 +88,20 @@ var sentencesController = function (app) {
     }
 
     function getSentenceByIdSentence(req, res, next) {
-        next('not yet implemented');
+        if (!req.params.sentence_id) {
+            next('missing sentence_id');
+            return;
+        }
+
+        app.models.sentences.getSentenceByIdSentence(req.params.sentence_id)
+            .then(function (sentence) {
+                console.log('got these sentences from es', sentence);
+                res.send(sentence);
+            })
+            .catch(function (err) {
+                next(err);
+            })
+            .done();
     }
 
     function getSentencesCreatedByUser(req, res, next) {
@@ -87,7 +109,127 @@ var sentencesController = function (app) {
     }
 
     function createSentence(req, res, next) {
-        next('not yet implemented');
+        var sentence = {};
+        var newSentence;
+        var languageEnglishName;
+        var userId;
+        var propertiesRequiredToCreateNewSentence;
+
+        async.series([
+            //get userId
+            function (callback) {
+                //todo: make userId come from jwt
+                userId = 1;
+                callback();
+            },
+            //remove idUser from propertiesRequiredToCreateNewSentence
+            function (callback) {
+                propertiesRequiredToCreateNewSentence = app.models.sentences.propertiesRequiredToCreateNewSentence;
+
+                if (!userId) {
+                    callback();
+                    return;
+                }
+
+                var userIdIndex = propertiesRequiredToCreateNewSentence.indexOf('idUser');
+                if (userIdIndex > -1) {
+                    propertiesRequiredToCreateNewSentence.splice(userIdIndex, 1);
+                }
+
+                callback();
+            },
+            //get the language that this sentence is in
+            function (callback) {
+                if (!req.query.language) {
+                    callback();
+                    return;
+                }
+
+                if (typeof req.query.language === 'string') {
+                    try {
+                        req.query.language = parseInt(req.query.languages);
+                    } catch (exception) {
+                        callback(exception);
+                        return;
+                    }
+                }
+
+                if (typeof req.query.language !== 'number') {
+                    callback('bad type for languages');
+                }
+
+
+                app.models.languages.getLanguagesEnglishNameFromIdLanguage(req.query.language)
+                    .then(function (langEnglishName) {
+                        languageEnglishName = langEnglishName;
+                        callback();
+                    })
+                    .catch(function (err) {
+                        callback(err);
+                    })
+                    .done();
+
+            },
+            //check if all required fields are present in request
+            function (callback) {
+                var missingRequiredPropertiesToCreateNewSentence = [];
+
+                propertiesRequiredToCreateNewSentence.forEach(function (propertyRequiredToCreateNewSentence) {
+                    if (!req.body[propertyRequiredToCreateNewSentence]) {
+                        missingRequiredPropertiesToCreateNewSentence.push(propertyRequiredToCreateNewSentence);
+                    }
+                });
+
+                if (missingRequiredPropertiesToCreateNewSentence.length) {
+                    callback('Missing the following required fields: ' + missingRequiredPropertiesToCreateNewSentence);
+                } else {
+                    callback();
+                }
+            },
+            //create sentence object
+            function (callback) {
+                app.models.sentences.propertiesThatCanBeSetWhenCreatingNewSentence.forEach(function (sentenceProperty) {
+                    if (req.body[sentenceProperty]) {
+                        sentence[sentenceProperty] = req.body[sentenceProperty];
+                    }
+                });
+
+                callback();
+            },
+            //set idUser in sentence if possible
+            function (callback) {
+                var idUserIndex = app.models.sentences.propertiesThatCanBeSetWhenCreatingNewSentence.indexOf(idUser);
+                if (userId && idUserIndex > -1) {
+                    sentence.idUser = userId;
+                }
+
+                callback();
+            },
+            //set sentence as active
+            function (callback) {
+                sentence.isActive = true;
+                callback();
+            },
+            //insert sentence into db
+            function (callback) {
+                app.models.sentences.indexNewSentence(languageEnglishName, sentence)
+                    .then(function (results) {
+                        newSentence = results;
+                        callback();
+                    })
+                    .catch(function (err) {
+                        callback(err);
+                    })
+                    .done();
+
+            }
+        ], function (err) {
+            if (err) {
+                next(err);
+            } else {
+                res.send(newSentence);
+            }
+        });
     }
 
     function updateSentence(req, res, next) {
