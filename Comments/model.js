a027cd12ac2e1bd4b951ab1740fdd19f77028333
brandon <brandon_charest@yahoo.com>
2016-02-15 15:13:51 -0500

    implement function to get comment from id

--- Comments/model.js
+++ Comments/model.js
@@ -1,3 +1,25 @@
-/**
- * Created by Brandon on 2/13/2016.
- */
+var q= require('q');
+
+var commentModel= function (app) {
+    var returnObject = {
+        //db queries
+        getAllFromComment: "SELECT * FROM comment",
+        getAllFromCommentWhereIdCommentMatches: "SELECT * FROM comment WHERE idComment=?",
+        createNewComment: "INSERT INTO comment SET ?",
+        updateComment: "UPDATE comment SET ? WHERE idComment=?",
+
+        //record properties
+        propertiesThatCanBeSetWhenCreatingNewComment: ['idComment', 'idUser', 'idSentence', 'description'],
+        propertiesRequiredToCreateNewComment:['idComment', 'idUser', 'idSentence', 'descrption'],
+        propertiesThatCanBSetWhenUpdatingComment: ['idComment', 'idUser', 'idSentence', 'description',
+            'likes','dislikes', 'isActive'],
+        propertiesRequiredToUpdateComment: [],
+
+        //functions
+        //todo find correct function name and finish function
+        getCommentFromIdComment: getCommentFromIdComment
+    };
+
+
+};
+module.exports=commentModel;
