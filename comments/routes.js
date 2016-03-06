db29d297f0b0cc6d175b58b8cc1f7fe864666447
Nickolas Freitas <n_freitas1@salemstate.edu>
2016-03-06 19:32:39 +0000

    Change 'Comments' directory to 'comments'

---
+++ comments/routes.js
@@ -0,0 +1,13 @@
+var router = function (app) {
+
+    app.get('/comments', app.controllers.comments.getAllComments);
+
+    app.get('/comments/:comment_id', app.controllers.comments.getCommentByIdComment);
+
+    app.post('/comments', app.controllers.comments.creatComment);
+
+    app.put('/comments/:comment_id', app.controllers.comments.updateComment);
+
+};
+
+module.exports=router;
